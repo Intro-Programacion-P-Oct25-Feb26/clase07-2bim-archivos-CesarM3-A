6c8c1cdc35eb9a8970ef53e2c0cc2afa192b57e1
CesarM3-A <cesarmateoab2007@gmail.com>
2026-01-28 12:46:04 -0500

    Lectura04

---
+++ ManejoArchivos/src/lectura04/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lectura04;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+    public static void main(String[] args) {
+        Procesos.leerPromediosDatosDos();
+    }
+}
